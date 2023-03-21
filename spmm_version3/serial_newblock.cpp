4dc6bc7852d2478a5f837d4d9f72fe621d21028a
Pengyu_Cheng <114738620+PengyuCheng0423@users.noreply.github.com>
2023-03-21 11:29:18 +0800

    Add files via upload

--- spmm_version3/serial_newblock.cpp
+++ spmm_version3/serial_newblock.cpp
@@ -6,10 +6,11 @@
 #include<unordered_map>
 #include "csr.h"
 #include "bitmap.h"
-// #include "transmat.h"
-// #include "v8sort.h"
+#include "transmat.h"
+#include "v8_sort.h"
 // #include "wbsort.h"
 
+#define MAXLENGTH 10000000
 bool origin = 1;
 
 using namespace std;
@@ -108,123 +109,116 @@ void gen_serail(SpM &mr,SpM &new_mr,unordered_map<int,int> &seq_dict){
 }
 
 void gen_trace_formats(SpM &mr,vector<int> &seq_input,vector<int> &rseq,vector<int> &sseq,SpM &smr_out,int &bnum,bool bitmap= 0,bool v8= 0,bool v8_sort= 0,bool serial= 0){
-    int* seq_bitmap;
+    int* seq_bitmap = new int;
     SpM mr_bitmap;
     if(bitmap){
-        int sect = 2048;
+        int sect = 8;
         seq_bitmap = bitmap_reorder(mr,sect);
-        // mr_bitmap = reorder_row(mr,seq_bitmap);
+        mr_bitmap = reorder_row(mr,seq_bitmap);
     }
     else for(int i = 0;i < sizeof(seq_bitmap)/sizeof(*seq_bitmap);i++) seq_bitmap[i]=i;
-    for(int i = 0;i < sizeof(seq_bitmap)/sizeof(*seq_bitmap);i++) cout << seq_bitmap[i];
+    // mr_bitmap.check();
+    // for(int i = 0;i < mr.shape[0];i++) cout << seq_bitmap[i]<<" ";
 
-    // vector<int> seq_v8_block;
-    // int offset =0;
-    // vector<unordered_map<int,int>> bseq_list;
-    // vector<int> bserial_colidx;
-    // vector<double> bserial_data;
-    // vector<int> bserial_indptr = {0};
-    // int indptr_offset = 0;
+    vector<int> seq_v8_block;
+    int offset =0;
+    vector<unordered_map<int,int>> bseq_list;
+    vector<int> bserial_colidx;
+    vector<double> bserial_data;
+    vector<int> bserial_indptr = {0};
+    int indptr_offset = 0;
 
-    // int panel_size = 2048;
+    int panel_size = 2048;
 
-    // vector<SpM> regions;
-    // vector<int> bsize_list{0};
+    vector<SpM> regions;
+    vector<int> bsize_list{0};
 
-    // // cout << "check mr_bitmap" << endl;
-    // // mr_bitmap.check();
+    cout<<"enter gen_new_panels"<<endl;
+    gen_new_panels(mr_bitmap,regions,bsize_list,bnum);
+    cout<<"out gen_new_panels"<<endl;
+    regions[0].check();
 
-    // gen_new_panels(mr_bitmap,regions,bsize_list,bnum);
-    // // for(auto i : regions[0].indices) cout<<i<<" ";
-    // // for(auto i: regions[0].indptr) cout<<i<<" ";
+    int cnt = 0;
+    vector<vector<int>> spv8_lists;
+    vector<vector<int>> panelsize_list;
+    vector<vector<int>> seq_order;
 
-    // int cnt = 0;
-    // vector<vector<int>> spv8_lists;
-    // vector<vector<int>> panelsize_list;
-    // vector<vector<int>> seq_order;
-    // for(auto r = regions.begin();r!=regions.end();r++){
-    //     // for(auto i : (*r).indices) cout<<i<<" ";
-    //     // for(auto i: (*r).indptr) cout<<i<<" ";
-    //     vector<int> seq_v8;
-    //     vector<int> spv8_list;
-    //     if(v8){
-    //         vector<int> add_panelsize_list = gen_panel_list(*r);
+    for(int index = 0;index < regions.size();index++){
+        vector<int> seq_v8;
+        vector<int> spv8_list;
+        if(v8){
+            vector<int> add_panelsize_list;
+            //返回一个add_panelsize_list的长度
+            add_panelsize_list = gen_panel_list(regions[index]);
 
-    //         // cout << "check *r" << endl;
-    //         // (*r).check();
+            panel_sort_nnz(seq_v8,spv8_list,regions[index],add_panelsize_list);
 
-    //         panel_sort_nnz(seq_v8,spv8_list,*r,add_panelsize_list);
-            
-    //         // cout << "check spv8_list\n";
-    //         // for(auto each:spv8_list) cout << each << ' ';
-    //         // cout << endl;
+            int *seq_v8_arr = new int[seq_v8.size()];
+            for(int i = 0;i < seq_v8.size();i++) seq_v8_arr[i] = seq_v8[i];
 
-    //         // cout << "check panelsize_list\n";
-    //         // for(auto each:add_panelsize_list) cout << each << ' ';
-    //         // cout << endl;
-            
+            SpM tmp_r = reorder_row(regions[index],seq_v8_arr);
 
-    //         SpM tmp_r = reorder_row(*r,seq_v8);
+            delete[] seq_v8_arr;seq_v8_arr = NULL;
 
-    //         // cout << "check tmp_r\n";
-    //         // tmp_r.check();
+            regions[index] = transpose_spv8_nnz(tmp_r,spv8_list,add_panelsize_list);
+            tmp_r.check();
 
-    //         // cout << "tmp_r_indices_size:\n";
-    //         // cout << tmp_r.indices.size() << '\n';
-    //         // cout << "tmp_r_rowptr:\n";
-    //         // for(auto each:tmp_r.indptr) cout << each << ' '; cout << endl;
+            vector<int> ex_in;            
+            ex_in.assign(add_panelsize_list.begin(),add_panelsize_list.end()-1);
+            panelsize_list.push_back(ex_in);
+        }
+        else{
+            if(v8_sort){
+                panel_sort(seq_v8,spv8_list,regions[index],panel_size);
+                int *seq_v8_arr = new int[seq_v8.size()];
+                for(int i = 0;i < seq_v8.size();i++) seq_v8_arr[i] = seq_v8[i];
+                regions[index] = reorder_row(regions[index],seq_v8_arr);
+                delete[] seq_v8_arr;seq_v8_arr = NULL;
+            }
+            else{
+                spv8_list = {};
+                seq_v8.resize(regions[index].shape[0]);
+                for(int i = 0;i < seq_v8.size();i++) seq_v8[i] = i;
+            }
+        }
+        for(auto iter = seq_v8.begin();iter!=seq_v8.end();iter++) seq_v8_block.push_back(*iter + bsize_list[cnt]);
+        for(int i = 0;i < regions[index].shape[2];i++) bserial_data.push_back(regions[index].data[i]);
+        for(int i = 1;i < regions[index].shape[0] - 1;i++) bserial_indptr.push_back(regions[index].indptr[i]+indptr_offset);
+        indptr_offset = indptr_offset + regions[index].indptr[regions[index].shape[0]];
+        spv8_lists.push_back(spv8_list);
+        cnt = cnt + 1;
 
-    //         //transpose_spv8_nnz:
-    //         //bug here?输出的indice_size为75，应该和输入一样是85
-    //         //
-    //         *r = transpose_spv8_nnz(tmp_r,spv8_list,add_panelsize_list);
-    //         // cout<<(*r).indptr.size()<<" "<<(*r).indices.size()<<endl;
-            
-    //         // cout << "check transpose\n";
-    //         // (*r).check();
+        SpM smr;
+        unordered_map<int,int> seq;
+        vector<int> order;
+        if(serial){
+            if(origin) gen_serail_origin(regions[index],smr,seq,order);
+            else gen_serail(regions[index],smr,seq);
+        }
+        else{
+            smr = regions[index];
+            seq = {};
+            for(int i = 0;i < regions[index].shape[1];i++) seq[i] = i;
+        }
 
-    //         vector<int> ex_in;            
-    //         ex_in.assign(add_panelsize_list.begin(),add_panelsize_list.end()-1);
-    //         panelsize_list.push_back(ex_in);
-    //     }
-    //     else{
-    //         if(v8_sort){
-    //             panel_sort(seq_v8,spv8_list,*r,panel_size);
-    //             *r = reorder_row(*r,seq_v8);
-    //         }
-    //         else{
-    //             spv8_list = {};
-    //             seq_v8.resize(r->indptr.size()-1);
-    //             for(int i = 0;i < seq_v8.size();i++) seq_v8[i] = i;
-    //         }
-    //     }
-    //     for(auto iter = seq_v8.begin();iter!=seq_v8.end();iter++) seq_v8_block.push_back(*iter + bsize_list[cnt]);
-    //     for(auto iter = r->data.begin();iter != r->data.end();iter++) bserial_data.push_back(*iter);
-    //     for(int i = 1;i < r->indptr.size();i++) bserial_indptr.push_back(r->indptr[i]+indptr_offset);
-    //     indptr_offset = indptr_offset + r->indptr[r->indptr.size()-1];
-    //     spv8_lists.push_back(spv8_list);
-    //     cnt = cnt + 1;
+        seq_order.push_back(order);
+        bseq_list.push_back(seq);
 
-    //     SpM smr;
-    //     unordered_map<int,int> seq;
-    //     vector<int> order;
-    //     if(serial){
-    //         if(origin) gen_serail_origin(*r,smr,seq,order);
-    //         else gen_serail(*r,smr,seq);
-    //     }
-    //     else{
-    //         smr = *r;
-    //         seq = {};
-    //         for(int i = 0;i < r->shape[1];i++) seq[i] = i;
-    //     }
+        for(int cc = 0;cc < smr.shape[2]; cc++) bserial_colidx.push_back(smr.indices[cc] + offset);
+        offset = offset + seq.size();
+    }
+    double *bserial_data_arr = new double[bserial_data.size()];
+    for(int i = 0;i < bserial_data.size();i++) bserial_data_arr[i] = bserial_data[i];
+    int *bserial_colidx_arr = new int[bserial_colidx.size()];
+    for(int i = 0;i < bserial_colidx.size();i++) bserial_colidx_arr[i] = bserial_colidx[i];
+    int *bserial_indptr_arr = new int[bserial_indptr.size()];
+    for(int i = 0;i < bserial_indptr.size();i++) bserial_indptr_arr[i] = bserial_indptr[i];
 
-    //     seq_order.push_back(order);
-    //     bseq_list.push_back(seq);
+    smr_out = SpM(bserial_data_arr,bserial_colidx_arr,bserial_indptr_arr,mr.shape);
 
-    //     for(auto cc = smr.indices.begin();cc != smr.indices.end();cc++) bserial_colidx.push_back(*cc + offset);
-    //     offset = offset + seq.size();
-    // }
-    // smr_out = SpM(bserial_data,bserial_colidx,bserial_indptr,mr.shape);
+    delete[] bserial_data_arr; bserial_data_arr = NULL;
+    delete[] bserial_colidx_arr; bserial_colidx_arr = NULL;
+    delete[] bserial_indptr_arr; bserial_indptr_arr = NULL;
     // //这里是要一维的数组
     // vector<vector<int>> seq_temp = SerialSort_block(seq_bitmap,seq_v8_block,bseq_list,seq_order);
     // seq_input.resize(0);
@@ -239,11 +233,14 @@ void gen_trace_formats(SpM &mr,vector<int> &seq_input,vector<int> &rseq,vector<i
     // for(auto k : seq_order[cnt]){
     //     sseq.push_back(k);
     // }
+
+    delete[] seq_bitmap; seq_bitmap = NULL;
+
 }
 
 int main(){
     vector<string> mlist;
-    ifstream file("matrix.txt");
+    ifstream file("D:\\Users\\Desktop\\learning in XJTU\\VSCodeC++\\Spmm\\matrix.txt");
     string s;
     while(getline(file,s)){
         mlist.push_back(s);
@@ -252,7 +249,7 @@ int main(){
     for(auto mlist_iter = mlist.begin();mlist_iter != mlist.end();mlist_iter++){
         string mname = *mlist_iter;
         mname = get_mname(mname);
-        string path1 = "mat/mtx/";
+        string path1 = "D:\\Users\\Desktop\\learning in XJTU\\VSCodeC++\\Spmm\\mat\\mtx\\";
         string path2 = "/";
         string path3 = ".mtx";
         string mpath = path1+mname+path2+mname+path3;
